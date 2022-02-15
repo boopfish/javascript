effad00637826429c9faa32382e216382168d4cf
yanan <yanan@deloitte.com.cn>
2022-02-15 17:42:49 +0800

    update

---
+++ vue单文件组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <School></School>
+  <Student></Student>
+</template>
+
+<script>
+import School from "./School";
+import Student from "./Student";
+
+export default {
+  name: "App",
+  components: {School, Student}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue单文件组件/School.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ schoolName }}</h1>
+    <h1>{{ schoolAddress }}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      schoolName: "学校名称",
+      schoolAddress: "重庆渝中"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
